03cef611469b57f92edfbd4d44cbf65cb95834bf
learningbali <rumaytsa2@gmail.com>
2026-07-07 12:06:10 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  /* Menghapus konfigurasi gambar eksternal yang sensitif terhadap versi compiler Vercel */
 };
 
 module.exports = nextConfig;
