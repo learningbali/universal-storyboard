e64892941bfa83b79b803bc852503172f3af0db9
learningbali <rumaytsa2@gmail.com>
2026-07-07 11:58:11 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        // Menambahkan opsi font serif sinematik jika browser mendukung
+        // Menambahkan opsi font  serif sinematik jika browser mendukung
         serif: ['Georgia', 'ui-serif', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
       },
     },
