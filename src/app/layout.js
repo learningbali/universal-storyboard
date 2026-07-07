02fae478de0e2f974b220f983b5a56253e449008
learningbali <rumaytsa2@gmail.com>
2026-07-07 12:16:34 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <body class="antialiased">{children}</body>
+      <body className="bg-black text-white antialiased">{children}</body>
     </html>
   );
 }
